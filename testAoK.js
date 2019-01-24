c0448727478d318d38bf1c9d0a688b540c35eeda
Bludwarf <bludwarf@gmail.com>
2019-01-24 13:09:26 +0100

    Ingame test OK

--- testAoK.js
+++ testAoK.js
@@ -1,11 +1,30 @@
 'use strict';
 
-const {playerParent} = require('./lib/AoK');
+const {playerParent, players, setAllResources, stateWithSelectedUnit} = require('./lib/AoK');
 
-console.log(playerParent.__proto__);
-console.log(playerParent.player.__proto__);
-console.log(playerParent.player.resources.__proto__);
-console.log(playerParent.player.resources.bois);
 playerParent.player.resources.nourriture = 1000;
 playerParent.player.resources.bois = 1000;
-console.log(playerParent.player.resources.bois);
+
+players.elements.forEach(/** @type Player */ player => {
+    if (player) {
+        setAllResources(player.resources, 0);
+        player.resources.pop = 500;
+    }
+});
+
+
+// LOOP
+setInterval(function () {
+
+    const selectedUnit = stateWithSelectedUnit.selectedUnit;
+    if (selectedUnit) {
+        const unitPlayer = selectedUnit.player;
+        if (unitPlayer) {
+            // Ennemi
+            if (unitPlayer !== players.player1) {
+                selectedUnit.hp = 0;
+            }
+        }
+    }
+
+}, 50);
