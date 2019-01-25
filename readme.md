a5694e9254807236cac2d561abd9315b53c525af
Bludwarf <bludwarf@gmail.com>
2019-01-25 16:40:59 +0100

    ArrayStruct

--- readme.md
+++ readme.md
@@ -1,10 +1,13 @@
 # Installation
 
-RobotJS ne supporte pas plus loin que Node 8.
+[RobotJS][1] ne supporte pas plus loin que Node 8.
 
 ```
 nvm install 8.15.0
 nvm use 8.15.0
 ```
 
-Suivre ensuite [ce commentaire](https://github.com/Robot/robot-js/pull/20#issuecomment-315207337).
+Suivre ensuite [ce commentaire][2].
+
+[1]: http://getrobot.net/docs/node.html
+[2]: https://github.com/Robot/robot-js/pull/20#issuecomment-315207337
--- testAoK.js
+++ testAoK.js
@@ -2,37 +2,92 @@
 
 /** @type AoKModule */
 const aok = require('./lib/AoK');
+const {aokModule, ResourceType} = aok;
+const LOOP = false;
 
-aok.players.elements.forEach(/** @type Player */ player => {
+const player = aokModule.players.player1;
+
+aokModule.players.player1.name = 'Bludwarf';
+aokModule.players.player2.name = 'Jimenez';
+
+/**
+ * @param {[Unit]} units
+ */
+const unselectAll = (units) => units.forEach(unit => unit.selected = false);
+
+aokModule.players.all.forEach((/** @type Player */player, i) => {
     if (player) {
-        if (player === aok.players.player1) {
-            aok.setAllResources(player.resources, 10000);
+        // Ressources
+        if (player === aokModule.players.gaia) {
+            // NOP
+            console.log('On ne touche pas aux ressources de Gaia');
+        }
+        if (player === aokModule.players.player1) {
+            aokModule.setAllResources(player.resources, 10000);
             player.resources.pop = 0;
             player.resources.popRestante = 500;
         } else {
-            aok.setAllResources(player.resources, 0);
+            aokModule.setAllResources(player.resources, 0);
             player.resources.pop = 500;
             player.resources.popRestante = 0;
         }
+
+        // Unités
+        console.log('Le player #'+i+' a '+player.stateElse.units.length+' unités');
     }
 });
 
+let currentUnitIndex = -1;
+/** @type Unit */
+let currentUnit = undefined;
 
-// LOOP
-setInterval(function () {
+// console.log(aokModule.players.gaia.stateElse.units.length);
 
-    const selectedUnit = aok.stateWithSelectedUnit.selectedUnit;
-    if (selectedUnit) {
-        const unitPlayer = selectedUnit.player;
-        if (unitPlayer) {
-            // Ennemi
-            if (unitPlayer !== aok.players.player1) {
-                selectedUnit.hp = 0;
-            } else {
-                // Bibi
-                selectedUnit.hp = selectedUnit.type.hp;
-            }
-        }
+// units.forEach(unit => console.log(unit.hp));
+
+const cycledUnits = aokModule.players.player1.stateElse.units;
+unselectAll(cycledUnits);
+function cycleSelect() {
+    if (currentUnit) {
+        // currentUnit.selected = false;
     }
 
-}, 50);
+    currentUnitIndex = (currentUnitIndex + 1) % cycledUnits.length;
+    currentUnit = cycledUnits[currentUnitIndex];
+    if (currentUnit) {
+        console.log("Select unit "+currentUnitIndex, {hp: currentUnit.hp, x: currentUnit.x, y: currentUnit.y});
+        // currentUnit.selected = true;
+    } else {
+        console.error('No unit '+currentUnitIndex);
+    }
+}
+
+cycledUnits[5].resourceCount = 10000;
+cycledUnits[5].resourceType = ResourceType.FOOD;
+
+// Passage en revue des unités possédées
+setInterval(() => {
+
+    cycleSelect();
+}, 1000);
+
+// LOOP
+if (LOOP) {
+    setInterval(function () {
+
+        const selectedUnit = aokModule.stateWithSelectedUnit.selectedUnit;
+        if (selectedUnit) {
+            const unitPlayer = selectedUnit.player;
+            if (unitPlayer) {
+                // Ennemi
+                if (unitPlayer !== aokModule.players.player1) {
+                    selectedUnit.hp = 0;
+                } else {
+                    // Bibi
+                    selectedUnit.hp = selectedUnit.type.hp;
+                }
+            }
+        }
+
+    }, 50);
+}
